fb956843d9cc7640d8acc4adde955f0b3349b708
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2020-07-07 17:39:20 -0300

    feat(test): only test package B

--- packages/packageB/src/index.ts
+++ packages/packageB/src/index.ts
@@ -1,8 +1,15 @@
 import { fnA } from '@test/packageA';
 
+export const anotherFn = () => {
+  console.log('another one and again');
+}
+
 export const fnB = () => {
   const value = fnA();
   console.log('packageB');
+
+  // add here to cause findRelatedTests to work
+  anotherFn();
 
   return value * 2;
 };
